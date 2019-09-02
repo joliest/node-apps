4943c8fc73b2cf7f3ced4eb66d1c9fdc56402f68
Joliver Estampador <joliverestampador@gmail.com>
2019-09-03 07:56:34 +0800

    Yargs 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -65,32 +65,3 @@ if (command === 'add') {
 }
 
 */
-
-/*6 
-    Using YARGS - to be able to parse a key/value pair argument from command line
-
-*/
-const yargs = require('yargs');
-
-// setting up a command. Can be viewed via --help
-yargs.command({
-    command: 'add',
-    describe: 'Adds a note',
-    handler: function() {
-        // executes this code when add is called
-        console.log('Adding a note . . . ');
-    }
-}).parse()
-
-yargs.command({
-    command: 'remove',
-    describe: 'Removes a note',
-    handler: function() {
-        // executes this code when remove is called
-        console.log("Removing a note . .. ");
-    }
-}).parse()
-
-
-// yargs has its own argv
-console.log(yargs.argv)
---
+++ notes-app/yargs-app.js
@@ -0,0 +1,45 @@
+
+
+/*6 
+    Using YARGS - to be able to parse a key/value pair argument from command line
+
+*/
+const yargs = require('yargs');
+
+// setting up a command. Can be viewed via --help
+yargs.command({
+    command: 'add',
+    describe: 'Adds a note',
+    // argv
+    builder: {
+        title: {
+            describe: 'Note title',
+            // make this required argument
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        // executes this code when add is called
+        console.log('Title . . . ' + argv.title);
+        console.log('Body . . . ' + argv.body);
+    }
+}).parse()
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: function() {
+        // executes this code when remove is called
+        console.log("Removing a note . .. ");
+    }
+}).parse()
+
+
+// yargs has its own argv
+// console.log(yargs.argv)
