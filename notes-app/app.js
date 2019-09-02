a19feac761cfb10c8f7300e6df4c8d840ea785bb
Joliver Estampador <joliverestampador@gmail.com>
2019-09-02 21:51:49 +0800

    Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -38,8 +38,59 @@ console.log('isEmail ::: ', validator.isEmail('jolisa.com'))
         3. Use it to print the string "Success!"
             to the console in green
         4. Test your work
-*/
+
 const notes = require('./notes.js');
 const chalk = require('chalk')
 console.log(notes.getNotes())
 console.log(chalk.green.bold('Success!'))
+
+console.log(process.argv[2])
+
+*/
+
+/*5. 
+    Accessing terminal argument via process.env
+
+const notes = require('./notes.js');
+const chalk = require('chalk');
+
+// value of 2 gets the third and exact value
+let command = process.argv[2];
+
+// node app.js add
+if (command === 'add') {
+    console.log('Adding notes..');
+} else if (command === 'remove') {
+    console.log('Removing notes..');
+}
+
+*/
+
+/*6 
+    Using YARGS - to be able to parse a key/value pair argument from command line
+
+*/
+const yargs = require('yargs');
+
+// setting up a command. Can be viewed via --help
+yargs.command({
+    command: 'add',
+    describe: 'Adds a note',
+    handler: function() {
+        // executes this code when add is called
+        console.log('Adding a note . . . ');
+    }
+}).parse()
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note',
+    handler: function() {
+        // executes this code when remove is called
+        console.log("Removing a note . .. ");
+    }
+}).parse()
+
+
+// yargs has its own argv
+console.log(yargs.argv)
