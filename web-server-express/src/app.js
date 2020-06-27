5f7f58d2febbd1ba3cacdf3508ff8c20b83b2c9c
Joliver Estampador <joliverestampador@gmail.com>
2020-06-27 15:18:58 +0800

    Using hbs

--- web-server-express/src/app.js
+++ web-server-express/src/app.js
@@ -6,10 +6,13 @@ const app = express();
 const PUBLIC_FOLDER = path.join(__dirname, '../public'); // returns path/src
 
 
-app.set('view engine', 'hbs')
-
 // runs html file
 app.use(express.static(PUBLIC_FOLDER));
+
+app.set('view engine', 'hbs')
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 // start server up, single time
 app.listen(3000, () => {
