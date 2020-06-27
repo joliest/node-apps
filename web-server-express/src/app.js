b0144c7a8ec0d60f9a86945cf11063773c920a46
Joliver Estampador <joliverestampador@gmail.com>
2020-06-27 15:55:29 +0800

    Adding partial path

--- web-server-express/src/app.js
+++ web-server-express/src/app.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express = require('express'); // returns a function
+const hbs = require('hbs');
 
 
 const app = express();
 const PUBLIC_FOLDER = path.join(__dirname, '../public'); // returns path/src
-const viewsPath = path.join(__dirname, '../template');
+const viewsPath = path.join(__dirname, './template/views');
+const partialsPath = path.join(__dirname, './template/partials');
 
 // static directories
 app.use(express.static(PUBLIC_FOLDER));
@@ -12,6 +14,7 @@ app.use(express.static(PUBLIC_FOLDER));
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 
 app.get('/', (req, res) => {
